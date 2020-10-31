3b67878fd7947c5f28c3f6a45b8c24e5bb718dde
Stephen Brown <steve@evolvedlight.co.uk>
2020-11-01 00:07:24 +0100

    Initial basic version

---
+++ BadiInfoScraper.cs
@@ -0,0 +1,47 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BadiStatusGraphsCreator
+{
+    public class BadiInfoScraper
+    {
+        public async Task<IEnumerable<BadiInfo>> GetBadiInfo()
+        {
+            var url = "https://www.stadt-zuerich.ch/ssd/de/index/sport/schwimmen/wassertemperaturen.html";
+            var web = new HtmlWeb();
+            var doc = await web.LoadFromWebAsync(url);
+
+
+            var headers = doc.GetElementbyId("baederinfossummary").Descendants("thead").Single().Descendants("th").Select(x => x.InnerText).ToList();
+            var results = new List<BadiInfo>();
+            foreach (var badiTableRow in doc.GetElementbyId("baederinfossummary").Descendants("tbody").Single().Descendants("tr"))
+            {
+                var badi = new BadiInfo();
+
+                var fields = badiTableRow.Descendants("td").Select(x => x.InnerText.Trim()).ToList();
+
+                badi.Name = fields[headers.IndexOf("Badeanlage")];
+                badi.Status = fields[headers.IndexOf("Status")];
+                badi.Temperature = int.Parse(fields[headers.IndexOf("Wasser")].Substring(0, 2));
+                badi.LastUpdate = DateTime.ParseExact(fields[headers.IndexOf("aktualisiert")], "dddd dd. MMMM yyyy HH.mm", new CultureInfo("de-DE"));
+                results.Add(badi);
+            }
+
+            return results;
+        }
+    }
+
+
+
+    public class BadiInfo
+    {
+        public string Name { get; set; }
+        public decimal Temperature { get; set; }
+        public string Status { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Octokit;
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using YetAnotherConsoleTables;
+
+namespace BadiStatusGraphsCreator
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var badiInfoScraper = new BadiInfoScraper();
+            var results = await badiInfoScraper.GetBadiInfo();
+
+            ConsoleTable.From(results).Write();
+
+            var github = new GitHubClient(new ProductHeaderValue("BadiInfoUpdater"));
+            var tokenAuth = new Credentials(Environment.GetEnvironmentVariable("GH_TOKEN"));
+
+            github.Credentials = tokenAuth;
+
+            var jsonContent = JsonSerializer.Serialize(results, new JsonSerializerOptions { WriteIndented = true, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
+
+            var existingFile = await github.Repository.Content.GetAllContents("evolvedlight", "ZhBadiStatus", "latest.json");
+
+            var upReq = new UpdateFileRequest("Update Badi Statuses", jsonContent, existingFile.First().Sha, "main");
+
+            var res = await github.Repository.Content.UpdateFile("evolvedlight", "ZhBadiStatus", "latest.json", upReq);
+            Console.WriteLine(res.Commit.Sha);
+        }
+    }
+}
